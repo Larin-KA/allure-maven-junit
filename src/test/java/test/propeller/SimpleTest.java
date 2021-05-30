c2cd3fe8a582d2b8831fdd71efb03b25144369f6
Kirill Larin <larinka.spbstu@gmail.com>
2021-05-30 13:49:21 +0300

    creating propellerads test project

---
+++ src/test/java/test/propeller/SimpleTest.java
@@ -0,0 +1,35 @@
+package test.propeller;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.junit.TextReport;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SimpleTest {
+    @Rule
+    public TextReport report = new TextReport();
+
+    @Before
+    public void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.startMaximized = true;
+    }
+    @Test
+    public void simpleMethod() {
+        open("https://yandex.ru");
+        $(By.xpath("//*[@aria-label=\"Запрос\"]")).val("propellerads").pressEnter();
+        $$(".serp-item").shouldHave(sizeGreaterThan(5));
+        $("#search-result").shouldHave(text("propellerads"));
+    }
+}
+
